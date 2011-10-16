8b5acb53c6478baea9dd76910197a1dcfdd6f819
mrrussellong <mrrussellong@gmail.com>
2011-10-16 14:48:22 +0800

    integration

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Jet::Application.routes.draw do
   root :to => 'pages#index'
+  resources :pages do
+    collection do
+      get :findClosestAirports, :findOriginAirports, :findDestinationAirports
+    end
+  end
 end
